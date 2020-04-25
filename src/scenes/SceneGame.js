fb0e3e9532386655368d3227b8978b6a1663da0f
MalgoskaBou <ghouga.m@gmail.com>
2020-04-25 22:06:45 +0200

    Change graphics and make size depend from scene size

--- src/scenes/SceneGame.js
+++ src/scenes/SceneGame.js
@@ -8,15 +8,22 @@ import {
   CELL_BCK,
 } from "../utils/const";
 import BulletsGroup from "../utils/Bullets";
-import { getImageSize } from "../utils/helpers";
+import {
+  getImageSize,
+  scaleTextToGameH,
+  scaleImgToGameH,
+} from "../utils/helpers";
 
 export default class SceneGame extends Phaser.Scene {
   preload() {
     this.load.image(BULLET, "img/laserBlue02.png");
-    this.load.image(TOILET_PAPER, "img/playerShip1_red.png");
-    this.load.image(VIRUS, "img/coin.png");
-    this.load.image(CELL, "img/flower.png");
+    this.load.image(VIRUS, "img/virus.png");
+    this.load.image(CELL, "img/cell.png");
     this.load.image(CELL_BCK, "img/cellBck.jpg");
+    this.load.spritesheet(TOILET_PAPER, "img/toilet_paper.png", {
+      frameWidth: 300,
+      frameHeight: 300,
+    });
 
     this.cursor = this.input.keyboard.createCursorKeys();
   }
@@ -24,18 +31,16 @@ export default class SceneGame extends Phaser.Scene {
   create() {
     this.points = 0;
 
-    this.cameras.main.setBackgroundColor(0x1d1923);
     this.addBckTiles();
     this.bulletGroup = new BulletsGroup(this);
     this.charactersGroup = this.physics.add.group();
 
-    this.addToiletPaper();
     this.addRandomCharacter();
-    this.pointsText = this.add.text(10, 10, "POINTS: 0", {
-      color: "#000000",
-      fontSize: this.cameras.main.width / 40,
-    });
+    this.addPointsText();
     this.addEvents();
+
+    this.loadToiletAnimation();
+    this.addToiletPaper();
 
     this.physics.add.collider(
       this.charactersGroup,
@@ -46,9 +51,73 @@ export default class SceneGame extends Phaser.Scene {
     );
   }
 
+  update() {
+    this.inputKeys.forEach((key) => {
+      if (Phaser.Input.Keyboard.JustDown(key)) {
+        this.fireBullet();
+      }
+    });
+
+    this.charactersGroup.children.iterate((child) => {
+      if (child?.y > this.cameras.main.height) {
+        child.destroy();
+        if (!child.isVirus) {
+          this.updatePoints();
+        }
+      }
+    });
+
+    if (this.cursor.left.isDown) {
+      this.toiletPaper.x -= MOVE_SPEED;
+    }
+    if (this.cursor.right.isDown) {
+      this.toiletPaper.x += MOVE_SPEED;
+    }
+  }
+
+  addBckTiles() {
+    const scale = 0.3;
+    this.add
+      .tileSprite(
+        0,
+        0,
+        this.cameras.main.width / scale,
+        this.cameras.main.height / scale,
+        CELL_BCK
+      )
+      .setOrigin(0)
+      .setScale(scale);
+  }
+
   updatePoints() {
     this.points++;
     this.pointsText.setText(`POINTS: ${this.points}`);
+  }
+
+  addPointsText() {
+    this.pointsText = this.add.text(10, 10, "POINTS: 0", {
+      color: "#000000",
+      fontSize: scaleTextToGameH(30, this),
+    });
+  }
+
+  addToiletPaper() {
+    const centerX = this.cameras.main.width / 2;
+    const bottom = this.cameras.main.height;
+    this.toiletPaper = this.add
+      .sprite(centerX, bottom - 90, TOILET_PAPER)
+      .setScale(scaleImgToGameH(10, TOILET_PAPER, this));
+  }
+
+  loadToiletAnimation() {
+    const config = {
+      key: "fire",
+      frames: this.anims.generateFrameNumbers(TOILET_PAPER),
+      frameRate: 14,
+      repeat: 0,
+    };
+
+    this.anims.create(config);
   }
 
   hitCharacter(character, bullet) {
@@ -61,26 +130,11 @@ export default class SceneGame extends Phaser.Scene {
     bullet.body.reset(0, 0);
   }
 
-  addBckTiles() {
-    this.add
-      .tileSprite(
-        0,
-        0,
-        this.cameras.main.width,
-        this.cameras.main.height,
-        CELL_BCK
-      )
-      .setOrigin(0);
-  }
-
-  addToiletPaper() {
-    const centerX = this.cameras.main.width / 2;
-    const bottom = this.cameras.main.height;
-    this.toiletPaper = this.add.image(centerX, bottom - 90, TOILET_PAPER);
-  }
-
   addCharacter(characterName) {
-    const characterScale = characterName === VIRUS ? 0.05 : 0.02;
+    const characterScale =
+      characterName === VIRUS
+        ? scaleImgToGameH(1.2, VIRUS, this)
+        : scaleImgToGameH(1, CELL, this);
 
     const characterWidth =
       getImageSize(characterName, this).width * characterScale;
@@ -109,6 +163,7 @@ export default class SceneGame extends Phaser.Scene {
 
   fireBullet() {
     this.bulletGroup.fireBullet(this.toiletPaper.x, this.toiletPaper.y - 20);
+    this.toiletPaper.play("fire");
   }
 
   addEvents() {
@@ -123,27 +178,5 @@ export default class SceneGame extends Phaser.Scene {
       callbackScope: this,
       loop: true,
     });
-  }
-
-  update() {
-    this.inputKeys.forEach((key) => {
-      if (Phaser.Input.Keyboard.JustDown(key)) {
-        this.fireBullet();
-      }
-    });
-
-    this.charactersGroup.children.iterate((child) => {
-      if (child?.y > this.cameras.main.height) {
-        child.destroy();
-        console.log(child.isVirus);
-      }
-    });
-
-    if (this.cursor.left.isDown) {
-      this.toiletPaper.x -= MOVE_SPEED;
-    }
-    if (this.cursor.right.isDown) {
-      this.toiletPaper.x += MOVE_SPEED;
-    }
   }
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,3 +2,12 @@ export const getImageSize = (imgKey, scene) => ({
   width: scene.textures.get(imgKey).getSourceImage().width,
   height: scene.textures.get(imgKey).getSourceImage().height,
 });
+
+export const scaleTextToGameH = (scale, context) =>
+  context.cameras.main.height / scale;
+
+export const scaleImgToGameH = (scalePercent, imgKey, context) => {
+  const scene = (context.cameras.main.height * scalePercent) / 100;
+  const img = (scene * 100) / getImageSize(imgKey, context).width;
+  return img / 10;
+};
