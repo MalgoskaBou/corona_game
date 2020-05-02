c4043338a66a44718aa0887f8b763e4c5ed34bd7
MalgoskaBou <ghouga.m@gmail.com>
2020-05-02 18:46:01 +0200

    Attept to repair pixels problem on mobile

--- src/game/config.js
+++ src/game/config.js
@@ -3,10 +3,12 @@ import SceneMain from "./scenes/mainScene";
 import SceneGame from "./scenes/SceneGame";
 import { isMobileDevice } from "./utils/isMobile";
 
+const dpr = window.devicePixelRatio;
+
 export const config = {
   isMobile: isMobileDevice(),
-  width: isMobileDevice() ? "100%" : 800,
-  height: "100%",
+  width: isMobileDevice() ? window.innerWidth * dpr : 800,
+  height: window.innerHeight * dpr,
   type: Phaser.AUTO,
   physics: {
     default: "arcade",
